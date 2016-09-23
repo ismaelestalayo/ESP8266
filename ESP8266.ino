9db4a45f87a7a75544c28199f0228b1f9cab3cbd
Ismael <ismael.em@outlook.com>
2016-09-24 01:28:02 +0200

    Add files via upload

---
+++ ESP8266.ino
@@ -0,0 +1,77 @@
+/*
+ *    AT+CWLAP                  (Scan Wifi)
+ *    AT+CWJAP="SSID","pass"    (Connect)
+ *    AT+CIFSR                  (/ipconfig)
+ *    AT+CIPMUX=1               (Multiple connections)
+ *    AT+CIPSERVER=1,80         (Server)
+ *    Example of line to parse: 
+*/
+char r;
+String R;
+
+void setup()  {
+  Serial.begin(9600);
+  Serial1.begin(115200);
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+   if (Serial1.available() ) {
+      r = Serial1.read();
+      if(r == 71){
+        webserver();
+        delay(500);
+      }
+      Serial.print(r);
+      R = R + r;
+   }
+ 
+  if (Serial.available() ) {
+     Serial1.write(Serial.read() );
+  }
+
+  if(r == '\n' && R.indexOf("GET /") > 0){
+    int start = R.indexOf("/");
+    int finish = R.indexOf("HTTP");
+    String command = R.substring(start+1, finish-1);
+    
+    if(command.equals("LED") )
+      digitalWrite(13, !digitalRead(13) );
+    
+    R = "";
+  }
+  
+  if(r == '\n')
+    R = "";
+  
+}
+
+//***************************************************************
+void http(String output){
+  Serial1.print("AT+CIPSEND=0,");              // AT+CIPSEND=0, num
+  Serial1.println(output.length());
+  if (Serial1.find(">")){                       // Si recibimos la peticion del mensaje
+    Serial1.println(output);            //Aqui va el string hacia el WIFI
+    delay(10);
+    while ( Serial1.available() > 0 ){
+      if (  Serial1.find("SEND OK") )    // Busca el OK en la respuesta del wifi
+        break;
+    }
+  }
+}
+
+void webserver(void){
+  http("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
+  http("<!DOCTYPE HTML>");
+  http("<html>");
+  http("<head><title>ESP8266</title></head>");
+  http("<body><font face=\"Segoe UI light\">");
+  http("<p float:left><img src=\"http://bilbaodynamics.com/wp-content/uploads/2016/01/BD_imagotipo_con_logo_4_v001-e1454764201240.png\" width = \"40%\"/></p>");
+  http("<h1>Probando el ESP8266</h1>");
+  http("<h4>(poner 192.168.0.X/comando)</h4>");
+  http("<ul><li>/LED</li><li>/comando2</li><li>/comando3</li></ul>");
+  http("</font></body></html>");
+  delay(10);
+  Serial1.println("AT+CIPCLOSE=0");
+}
+
