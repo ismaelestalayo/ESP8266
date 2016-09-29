e2373de94a80967b9055971decfd6749a40ec1ed
Ismael <ismael.em@outlook.com>
2016-09-29 23:21:36 +0200

    Version 1.0

--- ESP8266.ino
+++ ESP8266.ino
@@ -6,19 +6,39 @@
  *    AT+CIFSR                  (/ipconfig)
  *    AT+CIPMUX=1               (Multiple connections)
  *    AT+CIPSERVER=1,80         (Server)
- *    Example of line to parse: 
  *    Print something on serial: Serial.println("  ");
 */
+#include <PinchyWifi.h>
+#include <Servo.h>
+
+PinchyWifi Wifi;
+
 char r;
 String R;
 boolean b = false;
 
+Servo rueda1;
+Servo rueda2;
+
 void setup()  {
   Serial.begin(9600);
   Serial1.begin(115200);
+  rueda1.attach(5);
+  rueda2.attach(6);
   pinMode( 9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  //Setup for the servers and multiple connections.
+  digitalWrite(11, HIGH);
+  delay(3000);
+  Serial1.println("AT+CIPMUX=1");
+  delay(500);
+  Serial1.println("AT+CIPSERVER=1,80");
+  delay(500);
+  digitalWrite(11, LOW);
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
 }
 
 void loop(){
@@ -49,54 +69,106 @@ void loop(){
     R = "";
 
   if(b){
-    webServer();
+    Wifi.webServer();
     b = false;
   }
   
 }
 
-//***************************************************************
-void http(String output){
-  Serial1.print("AT+CIPSEND=0,");              // AT+CIPSEND=0, num
-  Serial1.println(output.length());
-  if (Serial1.find(">")){                       // Si recibimos la peticion del mensaje
-    Serial1.println(output);            //Aqui va el string hacia el WIFI
-    delay(10);
-    while ( Serial1.available() > 0 ){
-      if (  Serial1.find("SEND OK") )    // Busca el OK en la respuesta del wifi
+//****************************************************************************
+//****************************************************************************
+//****************************************************************************
+void commands(String command){
+  if(command.startsWith("LED_")){
+    if(command.equalsIgnoreCase("LED_rojo"))
+      digitalWrite(11, !digitalRead(11) );
+
+    else if(command.equalsIgnoreCase("LED_verde"))
+      digitalWrite(9, !digitalRead(9) );
+
+    else if(command.equalsIgnoreCase("LED_azul"))
+      digitalWrite(10, !digitalRead(10) );
+  }
+
+  else if(command.startsWith("adelante")){
+    String ss = command.substring(8);
+    int sec = ss.toInt();
+    rueda1.write(0);
+    rueda2.write(180);
+    switch (sec){
+      case 1:
+        delay(1000);
+        break;
+      case 2:
+        delay(2000);
+        break;
+      case 3:
+        delay(3000);
+        break;
+      case 4:
+        delay(4000);
+        break;
+      case 5:
+        delay(5000);
+        break;
+      case 6:
+        delay(6000);
+        break;
+      case 7:
+        delay(7000);
+        break;
+      case 8:
+        delay(8000);
+        break;
+      case 9:
+        delay(9000);
+        break;
+      default:
+        delay(1000);
         break;
     }
+    rueda1.write(90);
+    rueda2.write(90);
   }
-}
-
-void webServer(void){
-  http("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
-  http("<!DOCTYPE HTML>");
-  http("<html>");
-  http("<head><title>ESP8266</title></head>");
-  http("<body><font face=\"Segoe UI light\">");
-  http("<p float:left><img src=\"http://bilbaodynamics.com/wp-content/uploads/2016/01/BD_imagotipo_con_logo_4_v001-e1454764201240.png\" width = \"40%\"/></p>");
-  http("<h1>Probando el ESP8266</h1>");
-  http("<h4>(poner 192.168.0.X/comando)</h4>");
-  http("<ul><li>/r</li><li>/g</li><li>/b</li></ul>");
-  http("</font></body></html>");
-  delay(100);
-  Serial1.println("AT+CIPCLOSE=0");
-}
-//***************************************************************
-void commands(String command){
-  if(command.equalsIgnoreCase("red")
-    digitalWrite(11, !digitalRead(11) );
-  
-  else if(command.equalsIgnoreCase("green")
-    digitalWrite(9, !digitalRead(9) );
-
-  else if(command.equalsIgnoreCase("blue")
-    digitalWrite(10, !digitalRead(10) );
-
-  else if(command.startsWith("adelante"){
-    
+  else if(command.startsWith("atras")){
+    String ss = command.substring(5);
+    int sec = ss.toInt();
+    rueda1.write(180);
+    rueda2.write(0);
+    switch (sec){
+      case 1:
+        delay(1000);
+        break;
+      case 2:
+        delay(2000);
+        break;
+      case 3:
+        delay(3000);
+        break;
+      case 4:
+        delay(4000);
+        break;
+      case 5:
+        delay(5000);
+        break;
+      case 6:
+        delay(6000);
+        break;
+      case 7:
+        delay(7000);
+        break;
+      case 8:
+        delay(8000);
+        break;
+      case 9:
+        delay(9000);
+        break;
+      default:
+        delay(1000);
+        break;
+    }
+    rueda1.write(90);
+    rueda2.write(90);
   }
-  
 }
 
---
+++ PinchyWifi/PinchyWifi.h
@@ -0,0 +1,13 @@
+#ifndef PinchyWifi_h
+#define PinchyWifi_h
+
+#include "Arduino.h"
+
+class PinchyWifi
+{
+  public:
+    void http(String output);
+    void webServer(void);
+};
+
+#endif
